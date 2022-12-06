b37fee553bd09907cf9d7c80154926aa9a4ae016
zwmimi <zwmimi1212@gmail.com>
2022-12-06 11:18:47 +0900

    first-ci

---
+++ src/components/TodoList.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const todos = []
+
+const addTodo = (title: string) => {
+  console.log(title)
+}
+
+</script>
+
+<template>
+  <div>
+    <ul :style="{ listStyle: 'none' }">
+      <li
+        :style="{ backgroundColor: 'aliceblue', marginBottom: '10px' }"
+        v-for="todo in todos"
+      >{{ todo.title }}</li>
+    </ul>
+    <button @click="addTodo('add!')">Add</button>
+  </div>
+</template>
+
+<style scoped>
+</style>
